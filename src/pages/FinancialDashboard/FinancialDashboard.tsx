39713b04d4019a9fb05b1905ae36ee20026c3987
hasibul1670 <hasibulislam1670@gmail.com>
2024-08-23 22:26:47 +0600

    sd

---
+++ src/pages/FinancialDashboard/FinancialDashboard.tsx
@@ -0,0 +1,11 @@
+import FinancialDashboardV2 from '../../components/FinancialDashboad/FinancialDashboardV2';
+
+const FinancialDashboard = () => {
+  return (
+    <div>
+      <FinancialDashboardV2 />
+    </div>
+  );
+};
+
+export default FinancialDashboard;
